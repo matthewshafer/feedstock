5e9387908ee2e1b2fe5d2f0116257ff62e62431e
Matthew Shafer <matthewshafer@mac.com>
2010-02-07 08:00:26 +0000

    name change

---
+++ private/includes/cookieMonster.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @file
+* @author Matthew Shafer <matt@niftystopwatch.com>
+* 
+* @brief Handles creating/removing/checking of cookies
+*/
+class cookieMonster
+{
+	/**
+	* Constructor which grabs the client cookie and stores what it has locally
+	* 
+	* @param database that was created, this is used to invalidate the info from the database
+	*/
+	public function __construct($db)
+	{
+	
+	}
+	/**
+	* 
+	*/
+	public function createCookie()
+	{
+	
+	}
+	/**
+	* 
+	*/
+	public function removeCookie()
+	{
+	
+	}
+	/**
+	*
+	*/
+	public function checkCookie()
+	{
+	
+	}
+}
+?>
