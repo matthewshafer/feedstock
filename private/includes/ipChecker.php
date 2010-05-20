cb4da8c61348a3f72f776f145d19440e21bd75a7
Matthew Shafer <matthewshafer@mac.com>
2010-05-20 22:51:58 +0000

    I added some stuff to the validate IP which isn't used yet.  I feel like I need to implement it better.

--- private/includes/ipChecker.php
+++ private/includes/ipChecker.php
@@ -9,38 +9,43 @@
  class ipChecker
  {
  	private $ipAddress = null;
+ 	private $customHeader;
  	
  	
- 	public function __construct()
+ 	public function __construct($customHeader = null)
  	{
- 		
+ 		$this->customHeader = $customHeader;
  	}
  	
  	private function generateIP()
  	{
- 		if(!empty($_SERVER["HTTP_CLIENT_IP"]))
+ 	 	// REMOTE_ADDR header should always be set when we are looking for the ip address.  Apache sets it by default.
+ 	 	// the customHeader was added in order to specify your own header to grab the IP from
+ 		if($this->validIP($_SERVER["REMOTE_ADDR"]))
  		{
- 			$tmp = $_SERVER["HTTP_CLIENT_IP"];
- 			$tmp2 = explode(".", $tmp);
- 			
- 			if($this->validIP($tmp2))
- 			{
- 				$this->ipAddress = $tmp;
- 			}
+ 			$this->ipAddress = $_SERVER["REMOTE_ADDR"];
  		}
- 		
- 		if($this->ipAddress == null && !empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+ 		else if(!empty($_SERVER["X_REMOTE_IP"]) && $this->validIP($_SERVER["X_REMOTE_IP"]))
  		{
- 			$tmp = $_SERVER["HTTP_X_FORWARDED_FOR"];
- 			$tmp2 = explode(".", $tmp);
- 			
- 			if($this->validIP($tmp2))
- 			{
- 				$this->ipAddress = $tmp;
- 			}
+ 			$this->ipAddress = $_SERVER["X_REMOTE_IP"];
  		}
- 		
- 		if($this->ipAddress == null && !empty($_SERVER["REMOTE_ADDR"]))
+ 		else if(!empty($_SERVER["X_FORWARDED_FOR"]) && $this->validIP($_SERVER["X_FORWARDED_FOR"]))
+ 		{
+ 			$this->ipAddress = $_SERVER["X_FORWARDED_FOR"];
+ 		}
+ 		else
+ 		{
+ 			$this->ipAddress = null;
+ 		}
+ 	}
+ 	
+ 	private function getIPFromCustomHeader()
+ 	{
+ 		if(!empty($_SERVER[$this->customHeader]))
+ 		{
+ 			$this->ipAddress = $_SERVER[$this->customHeader];
+ 		}
+ 		else
  		{
  			$this->ipAddress = $_SERVER["REMOTE_ADDR"];
  		}
@@ -48,7 +53,14 @@
  	
  	public function checkIP($ip)
  	{
- 		$this->generateIP();
+ 		if($this->customHeader == null)
+ 		{
+ 			$this->generateIP();
+ 		}
+ 		else
+ 		{
+ 			$this->getIPFromCustomHeader();
+ 		}
  		$return = false;
  		
  		if($ip == $this->ipAddress)
@@ -60,16 +72,101 @@
  	}
  	
  	// need a lot better checking in here
- 	private function validIP($ipArr)
+ 	private function validIP()
  	{
+ 		$ipArr = explode(".", $this->ipAddress);
  		$return = false;
+ 		$keepSearching = true;
  		$ct = count($ipArr);
  		
  		if($ct == 4)
  		{
- 			if($ipArr[1] != "127")
+ 			// these check the private ranges to see if we are getting a private ip
+ 			// I could probably make this a lot faster but I need to come up with a way.
+ 			if(!$this->classSearch($this->ipAddress, "0.0.0.0", "255.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "0.0.0.0", "0.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "10.0.0.0", "10.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "127.0.0.0", "127.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "128.0.0.0", "128.0.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "169.254.0.0", "169.254.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "172.16.0.0", "172.31.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "191.255.0.0", "191.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "192.0.0.0", "192.0.0.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "192.168.0.0", "192.168.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching && $this->classSearch($this->ipAddress, "223.255.255.0", "223.255.255.255"))
+ 			{
+ 				$keepSearching = false;
+ 			}
+ 			
+ 			if($keepSearching)
  			{
  				$return = true;
+ 			}
+ 		}
+ 		
+ 		return $return;
+ 	}
+ 	
+ 	
+ 	private function classSearch($ipAddr, $startAddr, $finishAddr)
+ 	{
+ 		$startArr = explode(".", $startAddr);
+ 		$finishArr = explode(".", $finishAddr);
+ 		$ipArr = explode(".", $ipAddr);
+ 		$return = false;
+ 		
+ 		// I would totally do this recursively with another function but I need to find out how php handles recursion and if it uses a bunch more memory when things are recursive
+ 		if($ipArr[0] >= $startArr[0] && $ipArr[0] <= $finishArr[0])
+ 		{
+ 			if($ipArr[1] >= $startArr[1] && $ipArr[1] <= $finishArr[1])
+ 			{
+ 				if($ipArr[2] >= $startArr[2] && $ipArr[2] <= $finishArr[2])
+ 				{
+ 					if($ipArr[3] >= $startArr[3] && $ipArr[4] <= $finishArr[3])
+ 					{
+ 						$return = true;
+ 					}
+ 				}
  			}
  		}
  		
