e5058900b2531200e6365ab86020c48e9c29ef2d
Matthew Shafer <matthewshafer@mac.com>
2010-06-07 05:15:10 +0000

    Allows themes to flush the output so far.  It seems like it is good to flush after you generate the header but before the body so the client can grab the CSS and JS while we are generating the page.

---
+++ private/includes/outputHelper.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @file
+ * @author Matthew Shafer <matt@niftystopwatch.com>
+ * @brief Allows us to control output from the server while keeping all the output for static file caching
+ * 
+ */
+ 
+class outputHelper
+{
+	private $pageStore = "";
+	
+	/**
+	 * __construct function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->startBuffer();
+	}
+	
+	/**
+	 * startBuffer function.
+	 * 
+	 * @brief Starts an output buffer
+	 * @access private
+	 * @return void
+	 */
+	private function startBuffer()
+	{
+		ob_start();
+	}
+	
+	/**
+	 * flushBuffer function.
+	 * 
+	 * @brief stores the output and flushes it
+	 * @access public
+	 * @return void
+	 */
+	public function flushBuffer()
+	{
+		$tmp = ob_get_contents();
+		
+		ob_flush();
+		
+		if($tmp != false)
+		{
+			$pageStore = sprintf("%s%s", $this->pageStore, $tmp);
+		}
+		
+	}
+	
+	/**
+	 * stopStoreGetBuffer function.
+	 * 
+	 * @ 
+	 * @access public
+	 * @return String containing all the data from the buffer
+	 */
+	public function stopStoreGetBuffer()
+	{
+		$tmp = ob_get_contents();
+		
+		ob_end_flush();
+		
+		if($tmp != false)
+		{
+			$pageStore = sprintf("%s%s" $this->pageStore, $tmp);
+		}
+				
+		return $this->pageStore;
+	}
+}
+?>
