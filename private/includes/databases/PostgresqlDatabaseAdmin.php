24bc1b6d77196036f100c805a09758ff2c468d67
Matthew Shafer <matthewshafer@mac.com>
2011-10-01 13:06:56 -0700

    more additions to PostgresqlDatabaseAdmin

--- private/includes/databases/PostgresqlDatabaseAdmin.php
+++ private/includes/databases/PostgresqlDatabaseAdmin.php
@@ -33,7 +33,7 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 	
 	public function commitTransaction()
 	{
-		pg_query(parent::$this->databaseConnection, "COMMIT");
+		return pg_query(parent::$this->databaseConnection, "COMMIT");
 	}
 	
 	public function rollbackTransaction()
@@ -142,7 +142,22 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 	
 	public function getPostIdNiceCheckedTitle($niceTitle)
 	{
-	
+		$return = null;
+		
+		$formattedQuery = sprintf('SELECT "PrimaryKey" FROM %sposts WHERE "NiceTitle"=$1 LIMIT 1', parent::$this->tablePrefix);
+		
+		$result = $this->runQuery($formattedQuery, array($niceTitle));
+		
+		if(pg_num_rows($result) > 0)
+		{
+			$return = pg_fetch_result($result, "\"PrimaryKey\"");
+		}
+		else
+		{
+			throw new exception("Unable to get postID");
+		}
+		
+		return $return;
 	}
 	
 	public function checkDuplicateUri($type, $uri, $postOrPageId = null)
@@ -161,9 +176,9 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 		
 		if($formattedQuery !== null)
 		{
-			$result = $this->runQuery($formattedQuery);
+			$result = $this->runQuery($formattedQuery, array($uri));
 			
-			$id = pg_fetch_result($result, 0, "PrimaryKey");
+			$id = pg_fetch_result($result, "\"PrimaryKey\"");
 			
 			if($id !== false)
 			{
@@ -205,7 +220,7 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 		{
 			$result = $this->runQuery($formattedQuery, array($niceTitle));
 			
-			$id = pg_fetch_result($result, 0, "PrimaryKey");
+			$id = pg_fetch_result($result, "\"PrimaryKey\"");
 			
 			if($id !== false)
 			{
@@ -252,7 +267,7 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 		try
 		{
 			$result = $this->runQuery($query, array($cookieValue));
-			$return = pg_fetch_result($result, 0, "id");
+			$return = pg_fetch_result($result, 0, "\"id\"");
 		}
 		catch(exception $e)
 		{
@@ -302,6 +317,7 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 		foreach($categoryArray as $key)
 		{
 			$this->runQuery($formattedQuery, array($postId, $key));
+			// need to put a check here to see if we are actually inserting values
 		}
 	}
 	
@@ -312,7 +328,6 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 		$this->runQuery($formattedQuery, array($postID));
 	}
 	
-	/** not done yet **/
 	public function processTags($postId, $tagArray)
 	{	
 		if($tagArray !== null && !empty($tagArray))
@@ -324,15 +339,44 @@ class PostgresqlDatabaseAdmin extends PostgresqlDatabase implements GenericDatab
 			// loops through the tagArray and does al the work
 			foreach($tagArray as $key)
 			{
+				$catTagId = null;
 				$query1Result = $this->runQuery($formattedQuery1, array($key['NiceTitle']));
 				
 				// need to add the tag to the database
-				if(pg_affected_rows($query1Result === 0))
+				if(pg_affected_rows($query1Result) === 0)
 				{
-					$this->runQuery($formattedQuery2, array($key['Title'], $key['NiceTitle']));
+					$query2Result = $this->runQuery($formattedQuery2, array($key['Title'], $key['NiceTitle']));
+					
+					// making sure the previous query actually inserted something.
+					if(pg_affected_rows($query2Result) > 0)
+					{
+						// at this point the select should return something valid since we checked that the insert worked correctly we should get the PrimaryKey when we do a select
+						$query1Result = $this->runQuery($formattedQuery1, array($key['NiceTitle']));
+						// we have to put quotes around PrimaryKey because the postgresql library does not do this already
+						$catTagId = pg_fetch_result($query1Result, "\"PrimaryKey\"");
+						
+					}
+				}
+				else
+				{
+					// we have to put quotes around PrimaryKey because the postgresql library does not do this already
+					$catTagId = pg_fetch_result($query1Result, "\"PrimaryKey\"");
+				}
+				
+				// as long as we set the catTagId to something other than null it should be valid
+				if($catTagId !== null)
+				{
+					$query3Result = $this->runQuery($formattedQuery3, array($postId, $catTagId));
+					
+					if(pg_affected_rows($query3Result) === 0)
+					{
+						throw new exception("ProcessTags query3 failed somewhere");
+					}
 				}
 			}
 		}
+		
+		throw new exception("just for testing");
 	}
 	
 	public function getCorralList()
