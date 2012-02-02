6c928e7697fcc9668eaede0506e21d0cf5a8724e
Matthew Shafer <matthewshafer@mac.com>
2012-02-02 11:36:55 -0800

    More additions to the database migrations.  Need to change the name of the indexes and unify the case of everything which is going to mean changes to the sql queries in the database classes

--- db/migrate/20120201040229_initial_schema.rb
+++ db/migrate/20120201040229_initial_schema.rb
@@ -20,11 +20,56 @@ class InitialSchema < ActiveRecord::Migration
   		t.integer :Author, :null => false
   		t.datetime :Date, :null =>false
   		t.string :themeFile, {:null => false, :limit => 50}
-  		t.integer :Draft, {:null => false, :default = 1}
-  		t.string :Corral, {:default = nil, :limit => 50}
+  		t.integer :Draft, {:null => false, :default => 0}
+  		t.string :Corral, {:default => nil, :limit => 50}
   	end
   	add_index :pages, :Corral, :name => "CorralIndex"
   	add_index :pages, [:URI, :Draft], :name => "URIDraftINDEX"
+
+    create_table :posts, :primary_key => :PrimaryKey do |t|
+      t.integer :PrimaryKey
+      t.string :Title, :null => false
+      t.string :NiceTitle, :null => false
+      t.string :URI, :null => false
+      t.text :PostData, {:null => false, :limit => 16777216}
+      t.text :Category, :null => false
+      t.text :Tags, :null => false
+      t.integer :Author, :null => false
+      t.datetime :Date, :null => false
+      t.string :themeFile, {:null => false, :limit => 50}
+      t.integer :Draft, {:null => false, :default => 0}
+    end
+    add_index :posts, :URI, :name => "URIINDEX"
+    add_index :posts, [:Draft, :Date], :name => "Date_Draft"
+    add_index :posts, [:Draft, :PrimaryKey], :name => "DraftPrimaryIndex"
+
+    create_table :posts_tax, :id => false do |t|
+      t.integer :PostID, :null => false
+      t.integer :CatTagID, :null => false
+    end
+    add_index :posts_tax, :PostID, :name => "PostIDindex"
+    add_index :posts_tax, :CatTagID, :name => "CatTagIDindex"
+    add_index :posts_tax, [:PostID, :CatTagID], :name => "PostCatTagIndex"
+
+    create_table :snippet, :primary_key => :PrimaryKey do |t|
+      t.integer :PrimaryKey
+      t.string :Name, :null => false
+      t.text :SnippetData, {:null => false, :limit => 16777216}
+    end
+    add_index :snippet, :Name, :name => "NameIndex"
+
+    create_table :users, :primary_key => :id do |t|
+      t.integer :id
+      t.text :loginName, :null => false
+      t.text :displayName, :null => false
+      t.string :PasswordHash, {:null => false, :limit => 512}
+      t.string :Salt, :null => false
+      t.integer :Permissions, {:null => false, :default => 99}
+      t.integer :CanAdminUsers, {:null => false, :default => 0}
+      t.string :CookieVal, {:null => false, :limit => 512}
+    end
+
+
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,61 @@ ActiveRecord::Schema.define(:version => 20120201040229) do
   add_index "catstags", ["URIName"], :name => "URINameINDEX"
 
   create_table "pages", :primary_key => "PrimaryKey", :force => true do |t|
-    t.string "Title",                         :null => false
-    t.string "NiceTitle",                     :null => false
-    t.string "URI",                           :null => false
-    t.text   "PageData",  :limit => 16777216, :null => false
+    t.string   "Title",                                        :null => false
+    t.string   "NiceTitle",                                    :null => false
+    t.string   "URI",                                          :null => false
+    t.text     "PageData",  :limit => 16777216,                :null => false
+    t.integer  "Author",                                       :null => false
+    t.datetime "Date",                                         :null => false
+    t.string   "themeFile", :limit => 50,                      :null => false
+    t.integer  "Draft",                         :default => 0, :null => false
+    t.string   "Corral",    :limit => 50
+  end
+
+  add_index "pages", ["Corral"], :name => "CorralIndex"
+  add_index "pages", ["URI", "Draft"], :name => "URIDraftINDEX"
+
+  create_table "posts", :primary_key => "PrimaryKey", :force => true do |t|
+    t.string   "Title",                                        :null => false
+    t.string   "NiceTitle",                                    :null => false
+    t.string   "URI",                                          :null => false
+    t.text     "PostData",  :limit => 16777216,                :null => false
+    t.text     "Category",                                     :null => false
+    t.text     "Tags",                                         :null => false
+    t.integer  "Author",                                       :null => false
+    t.datetime "Date",                                         :null => false
+    t.string   "themeFile", :limit => 50,                      :null => false
+    t.integer  "Draft",                         :default => 0, :null => false
+  end
+
+  add_index "posts", ["Draft", "Date"], :name => "Date_Draft"
+  add_index "posts", ["Draft", "PrimaryKey"], :name => "DraftPrimaryIndex"
+  add_index "posts", ["URI"], :name => "URIINDEX"
+
+  create_table "posts_tax", :id => false, :force => true do |t|
+    t.integer "PostID",   :null => false
+    t.integer "CatTagID", :null => false
+  end
+
+  add_index "posts_tax", ["CatTagID"], :name => "CatTagIDindex"
+  add_index "posts_tax", ["PostID", "CatTagID"], :name => "PostCatTagIndex"
+  add_index "posts_tax", ["PostID"], :name => "PostIDindex"
+
+  create_table "snippet", :primary_key => "PrimaryKey", :force => true do |t|
+    t.string "Name",                            :null => false
+    t.text   "SnippetData", :limit => 16777216, :null => false
+  end
+
+  add_index "snippet", ["Name"], :name => "NameIndex"
+
+  create_table "users", :force => true do |t|
+    t.text    "loginName",                                    :null => false
+    t.text    "displayName",                                  :null => false
+    t.string  "PasswordHash",  :limit => 512,                 :null => false
+    t.string  "Salt",                                         :null => false
+    t.integer "Permissions",                  :default => 99, :null => false
+    t.integer "CanAdminUsers",                :default => 0,  :null => false
+    t.string  "CookieVal",     :limit => 512,                 :null => false
   end
 
 end
