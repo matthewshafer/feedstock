f848e03b87848d76503cda3f9dd6ec912c9a9f46
Matthew Shafer <matthewshafer@mac.com>
2012-06-21 00:19:08 -0700

    adjusting the order of things

--- private/themesAdmin/stock/footer.php
+++ private/themesAdmin/stock/footer.php
@@ -1,8 +1,10 @@
-	</div>
-	
-	</div>
-	<div id="footer">
-		For the latest version of Feedstock be sure to check <a href="http://niftystopwatch.com">niftystopwatch.com</a>
+      </div>
+    </div>
+
+    <footer class="footer">
+      For the latest version of Feedstock be sure to check <a href="http://niftystopwatch.com">niftystopwatch.com</a>
+    </footer>
+
 	</div>
 
   <script src="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/js/jquery-1.7.2.min.js'); ?>"></script>
--- private/themesAdmin/stock/index.php
+++ private/themesAdmin/stock/index.php
@@ -1,7 +1,5 @@
 <?php require_once("header.php"); ?>
-		<div id="Alpha Info">
 			Use the links above to do stuff.
 			I'll make this prettier and actually do some stuff in the future.
-		</div>
 <?php require_once("footer.php"); ?>
 
