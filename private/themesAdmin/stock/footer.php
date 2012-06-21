65d71feae51d3c1b9f1f87b25984856f953a1b6c
Matthew Shafer <matthewshafer@mac.com>
2012-06-20 23:48:08 -0700

    Navbar for admin page

--- private/themesAdmin/stock/footer.php
+++ private/themesAdmin/stock/footer.php
@@ -4,6 +4,9 @@
 	<div id="footer">
 		For the latest version of Feedstock be sure to check <a href="http://niftystopwatch.com">niftystopwatch.com</a>
 	</div>
+
+  <script src="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/js/jquery-1.7.2.min.js'); ?>"></script>
+  <script src="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/js/bootstrap.min.js'); ?>"></script>
 	
 	</body>
 </html>
--- private/themesAdmin/stock/header.php
+++ private/themesAdmin/stock/header.php
@@ -1,12 +1,62 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<Link href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/css/style.css'); ?>" rel="stylesheet" type="text/css">
+		<Link href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+		<Link href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/css/bootstrap-responsive.min.css'); ?>" rel="stylesheet" type="text/css">
+			<Link href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/css/extra.css'); ?>" rel="stylesheet" type="text/css">
 		<title><?php echo $this->templateEngine->getHtmlTitle();  ?></title>
 	</head>
 	
 	<body>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+
+					<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/'); ?>" id="logo">Feedstock Admin</a>
+
+					<div class="nav-collapse">
+						<ul class="nav pull-right">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Posts<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/posts'); ?>">View Posts</a>
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/post'); ?>">New Post</a>
+								</ul>
+							</li>
+
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/pages'); ?>">View Pages</a>
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/page'); ?>">New Page</a>
+								</ul>
+							</li>
+
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Snippets<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/snippets/'); ?>">View Snippets</a>
+									<a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/snippet'); ?>">New Snippet</a>
+								</ul>
+							</li>
+
+							<li><a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/tags'); ?>">Tags</a></li>
+							<li><a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/categories'); ?>">Categories</a></li>
+							<li><a href="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php/corral'); ?>">Corral</a></li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		</div>
 	<div id="page">
 	
 	<div id="head">
