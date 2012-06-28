fecbf935ed14959cd011e31055d2da55decfa6c1
Matthew Shafer <matthewshafer@mac.com>
2012-06-28 02:48:35 -0700

    changes to createPost.php

--- private/themesAdmin/stock/createPost.php
+++ private/themesAdmin/stock/createPost.php
@@ -1,36 +1,72 @@
 <?php require_once("header.php"); ?>
 			<form name="createPost" class="well" method="post" action="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php'); ?>">
-				Post Title: (We create a URI based off this)<br>
-				<input name="postTitle" type="text" class="span5" value="<?php echo $this->templateEngine->postTitleId(); ?>"><br>
-				Post Body: <br>
-				<textarea class="span5" name="postorpagedata"><?php echo $this->templateEngine->postBodyId(); ?></textarea>
-				<br>
-				Categories: (This is going to be a formatted list where you check boxes.)<br>
-				<!-- <input name="postCategories" type="text" value="<?php echo $this->templateEngine->postCategoriesId(); ?>"><br> -->
+				<div class="control-group">
+					<label class="control-label"><h4>Post Title: (We create a URI based off this)</h4></label>
+					<input name="postTitle" type="text" class="span5" value="<?php echo $this->templateEngine->postTitleId(); ?>">
+				</div>
+				<div class="control-group">
+					<label class="control-label"><h4>Post Body:</h4></label>
+					<textarea class="span5" rows="10" name="postorpagedata"><?php echo $this->templateEngine->postBodyId(); ?></textarea>
+				</div>
+				
+				<div class="control-group">
+					<label class="control-label"><h4>Categories:</h4></label>
+					<div class="controls">
+						<table class="table table-condensed">
+							<thead>
+								<th>Selected</th>
+								<th>Name</th>
+							</thead>
+							<tbody>
 				<?php foreach($this->templateEngine->getCategoryData() as $key)
 				{
+					echo '<tr>';
+
 					if(isset($key["Checked"]) and $key["Checked"] == 1)
 					{
-						echo '<input name="postCategories[]" type="checkbox" value="' . $key["PrimaryKey"] . '" checked>' . $key["Name"] . '<br>' . "\n\t\t\t\t";
+						echo '<td><input name="postCategories[]" type="checkbox" value="' . $key["PrimaryKey"] . '" checked></td>' . '<td>' . $key["Name"] . '</td>';
 					}
 					else
 					{
-						echo '<input name="postCategories[]" type="checkbox" value="' . $key["PrimaryKey"] . '">' . $key["Name"] . '<br>' . "\n\t\t\t\t";
+						echo '<td><input name="postCategories[]" type="checkbox" value="' . $key["PrimaryKey"] . '"></td>' . '<td>' . $key["Name"] . '</td>';
 					}
+
+					echo '</tr>';
 				} 
 				echo "\n";
-				?>
-				Tags: (separate with a ,)<br>
-				<input name="postTags" type="text" class="textInput" value="<?php echo $this->templateEngine->postTagsId(); ?>"><br>
-				Draft: <br>
-				yes<input name="draft" type="radio" value="1" <?php echo $this->templateEngine->isDraft() == 1 ? "Checked" : ""; ?>>
-				no<input name="draft" type="radio" value="0" <?php echo $this->templateEngine->isDraft() == 0 ? "Checked" : ""; ?>><br>
-				Use current date: <br>
-				yes<input name="useCurrentDate" type="radio" value="1">
-				no<input name="useCurrentDate" type="radio" value="0" Checked>
+				?>	
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label"><h4>Tags: (separate with a ,)</h4></label>
+					<input name="postTags" type="text" class="textInput" value="<?php echo $this->templateEngine->postTagsId(); ?>">
+				</div>
+				<div class="control-group">
+					<label class="control-label"><h4>Draft:</h4></label>
+					<div class="controls">
+						<label class="radio inline">
+							<input name="draft" type="radio" value="1" <?php echo $this->templateEngine->isDraft() == 1 ? "Checked" : ""; ?>> yes
+						</label>
+						<label class="radio inline">
+							<input name="draft" type="radio" value="0" <?php echo $this->templateEngine->isDraft() == 0 ? "Checked" : ""; ?>> no
+						</label>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label"><h4>Use current date:</h4></label>
+					<div class="controls">
+						<label class="radio inline">
+							<input name="useCurrentDate" type="radio" value="1">yes
+						</label>
+						<label class="radio inline">
+							<input name="useCurrentDate" type="radio" value="0" Checked>no
+						</label>
+					</div>
+				</div>
 				<input name="type" type="hidden" value="postAdd">
 				<input name="id" type="hidden" value="<?php echo $this->templateEngine->postId(); ?>">
-				<br>
 				<input name="submit" type=submit value="Post" class="btn btn-primary">
 			</form>
 <?php require_once("footer.php"); ?>
