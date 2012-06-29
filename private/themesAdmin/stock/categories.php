152e756f019b44b10441ada492a998d040745390
Matthew Shafer <matthewshafer@mac.com>
2012-06-29 03:00:07 -0700

    Changes to the categories page

--- private/themesAdmin/stock/categories.php
+++ private/themesAdmin/stock/categories.php
@@ -1,19 +1,49 @@
 <?php require_once("header.php"); ?>
-			<form name="createCategory" method="post" action="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php'); ?>">
-				Category Title: (We create a URI based off this)<br>
-				<input name="categoryTitle" type="text" value="<?php echo $this->templateEngine->categoryTitleId(); ?>"><br>
-				<input name="type" type="hidden" value="categoryAdd">
-				<input name="id" type="hidden" value="<?php echo $this->templateEngine->categoryId(); ?>">
-				<input name="submit" type="submit" value="Post">
+			
+			<form name="createCategory" class="well form-inline" method="post" action="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php'); ?>">
+				<div class="control-group">
+					<label class="control-label"><h4>Category Name: (We create a URI based off this)</h4></label>
+				</div>
+				<div class="control-group">
+					<input name="categoryTitle" type="text" value="<?php echo $this->templateEngine->categoryTitleId(); ?>">
+					<input name="type" type="hidden" value="categoryAdd">
+					<input name="id" type="hidden" value="<?php echo $this->templateEngine->categoryId(); ?>">
+					<input name="submit" type="submit" class="btn btn-primary" value="Post">
+				</div>
 			</form>
-			<br><br>
+			
+			<div class="well">
+
+				<table class="table">
+					<thead>
+						<th>Delete(NYI)</th>
+						<th>Name</th>
+						<th>Uri</th>
+					</thead>
+					<tbody>
 			<?php
 			foreach($this->templateEngine->getCategoryData() as $key)
 			{
-				echo "Category: " . $key["Name"] . " " . "URI: " . $key["URIName"] . "<br><br>";
+				echo '<tr>';
+				echo '<td>';
+				printf('<input name="categoriesDelete[]" type="checkbox" value="%s">', $key["PrimaryKey"]);
+				echo '</td>';
+
+				echo '<td>';
+				echo $key["Name"];
+				echo '</td>';
+
+				echo '<td>';
+				echo $key["URIName"];
+				echo '</td>';
+
+				echo '</tr>';
 			}
 
 			//echo "<br><br> The array of all the data<br>";
 			//print_r($this->templateEngine->getCategoryData());
 			?>
+
+				</table>
+			</div>
 <?php require_once("footer.php"); ?>
