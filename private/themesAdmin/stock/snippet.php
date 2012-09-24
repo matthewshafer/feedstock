866caff5f694cec16d3aeccc00a84112d7e1c94c
Matthew Shafer <matthewshafer@mac.com>
2012-09-24 12:46:00 -0700

    a few additions to bootstrap admin

--- private/themesAdmin/stock/snippet.php
+++ private/themesAdmin/stock/snippet.php
@@ -1,19 +1,37 @@
 <?php require_once("header.php"); ?>
-<div id="snippitBox">
+<form name="deleteSnippets" class="well" method="post" action="<?php printf("%s%s", $this->templateEngine->getAdminUrl(), '/index.php'); ?>">
+
+<table class="table">
+	<thead>
+		<th>Delete</th>
+		<th>Snippet Info</th>
+	</thead>
+	<tbody>
 
 <?php
 
-foreach($this->templateEngine->getTheData() as $key)
-{
-	echo '<div class="singleCorralBox">';
+foreach($this->templateEngine->getTheData() as $key): ?>
+	<tr>
+		<td>
+	<?php
 	if(isset($key["PrimaryKey"]))
 	{
 		//echo '<a href="../page/'.$key["PrimaryKey"].'">';
+		printf('<input name="snippetDelete[]" type="checkbox" value="%s">', $key["PrimaryKey"]);
+		echo '</td>';
+		echo '<td>';
 		printf('<a href="../snippet/%s">%s</a>', $key["PrimaryKey"], $key["Name"]);
 	}
-	echo '</div>';
-}
-?>
+	?>
+			</td>
+	</tr>
 
-</div>
+<?php endforeach; ?>
+
+	</tbody>
+</table>
+
+		<input name="type" type="hidden" value="snippetRemove">
+		<input name="submit" type="submit" value="Delete" class="btn btn-danger">
+</form>
 <?php require_once("footer.php"); ?>
